e7283e068b9471a080eb6479fe66f40439bec700
Josef Bisits <jbisits@gmail.com>
2023-02-27 08:26:30 +1100

    Update src/SeawaterPolynomials.jl
    Co-authored-by: Navid C. Constantinou <navidcy@users.noreply.github.com>

--- src/SeawaterPolynomials.jl
+++ src/SeawaterPolynomials.jl
@@ -25,7 +25,7 @@ function thermal_sensitivity end
 """
     haline_sensitivity(Θ, Sᴬ, Z, equation_of_state)
 
-Returns the "Boussinesq haline contraction coefficient" for a seawater parcel with absolute
+Return the "Boussinesq haline contraction coefficient" for a seawater parcel with absolute
 salinity `Sᴬ`, at fixed conservative temperature `Θ`, and geopotential height `Z`, using
 the Boussinesq `equation_of_state`. The haline contraction coefficient is
 
