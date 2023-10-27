3154aab5aff94a9385421e616329ddc64989d8d2
Xin Kai Lee <50624521+xkykai@users.noreply.github.com>
2023-10-27 16:02:37 -0400

    Update src/SeawaterPolynomials.jl
    Co-authored-by: Gregory L. Wagner <gregory.leclaire.wagner@gmail.com>

--- src/SeawaterPolynomials.jl
+++ src/SeawaterPolynomials.jl
@@ -5,7 +5,7 @@ module SeawaterPolynomials
 
 Return the "Boussinesq thermal expansion coefficient" for a seawater parcel with
 conservative temperature `Θ`, at fixed absolute salinity `Sᴬ`, and geopotential height `Z`
-using the Boussinesq `equation_of_state`. The thermal expansion coefficient is
+using the Boussinesq `equation_of_state`. The thermal sensitivity coefficient is
 
 ```math
 a(Θ, Sᴬ, Z) = - \\left.\\frac{∂ρ}{∂Θ}\\right|_{Sᴬ, Z} ,
