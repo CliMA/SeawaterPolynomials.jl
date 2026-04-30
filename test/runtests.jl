59a1cbdd74f253c564c4317b5cd9788c75ccc4dc
Simone Silvestri <silvestri.simone0@gmail.com>
2026-04-30 16:29:33 +0200

    runtests

--- test/runtests.jl
+++ test/runtests.jl
@@ -144,22 +144,22 @@ end
     end
 end
 
-# @testset "convert" begin
-#     for (FT, FT2) in zip((Float32, Float64), (Float64, Float32))
-#         teos10 = instantiate_teos10_polynomial(FT)
-#         test10 = convert(FT2, teos10)
-#         @test eltype(teos10) == FT2
+@testset "convert" begin
+    for (FT, FT2) in zip((Float32, Float64), (Float64, Float32))
+        teos10 = instantiate_teos10_polynomial(FT)
+        test10 = convert(FT2, teos10)
+        @test eltype(teos10) == FT2
 
-#         for coefficient_set in (:Linear,
-#                                 :Cabbeling,
-#                                 :CabbelingThermobaricity,
-#                                 :Freezing,
-#                                 :SecondOrder,
-#                                 :SimplestRealistic)
+        for coefficient_set in (:Linear,
+                                :Cabbeling,
+                                :CabbelingThermobaricity,
+                                :Freezing,
+                                :SecondOrder,
+                                :SimplestRealistic)
 
-#             eos = instantiate_roquet_equation_of_state(FT, coefficient_set)
-#             eos = convert(FT2, teos10)
-#             @test eltype(teos10) == FT2
-#         end
-#     end
-# end
+            eos = instantiate_roquet_equation_of_state(FT, coefficient_set)
+            eos = convert(FT2, teos10)
+            @test eltype(teos10) == FT2
+        end
+    end
+end
